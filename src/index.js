87705f1096db3fe8926d3884d6b4cfc3353d4dfd
Rishabh Anand <rishabhanand42@gmail.com>
2020-01-27 19:10:54 +0530

    setup and test postgresql

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
 import routes from './routes';
+import Postgres from './models';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -13,5 +14,13 @@ app.use(urlencoded({ extended : true }));
 app.use(morgan('dev'));
 
 app.use('/api', routes);
+
+Postgres.sequelize.authenticate()
+.then(() => {
+    console.log("Successfully connected to Postgres");
+})
+.catch(err => {
+    console.log("Unable to connect to the database", err);
+});
 
 app.listen(PORT, () => console.log(`Parkpoolr API started on ::${PORT}`));
